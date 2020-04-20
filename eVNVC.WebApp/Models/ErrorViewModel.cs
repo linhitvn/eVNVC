011dd6448537da18183dc7000ad31b76b3a9b536
victor-vinova <victor@vinova.com.sg>
2020-04-20 15:34:12 +0700

    Add project files.

---
+++ eVNVC.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eVNVC.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
